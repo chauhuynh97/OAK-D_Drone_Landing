2a1fea72578746cb308d950cdfd97acc236a1649
Chau Huynh <45109612+chauhuynh97@users.noreply.github.com>
2022-01-26 17:42:46 -0800

    Add files via upload
    Add straight_land function. Assuming camera is always at [0, 0, 0] and drone is [0, takeoff height, 1].

--- Main.py
+++ Main.py
@@ -1,116 +1,68 @@
 import CoDrone
 from CoDrone import Direction
+
 print("Creating drone object")
 drone = CoDrone.CoDrone()
 print("Getting ready to pair")
 drone.pair(drone.Nearest)
 print("Paired!")
 
-height = drone.get_height()
-print(height)
-drone.takeoff()
 
-print("taking off")
+def straight_land(start_coords, land_coords = [0,0,0]):
+    height_threshold = 80 # height before drone.land()
+    # x:left-right, y:height, z:depth
+    xs,ys,zs = start_coords
+    xl,yl,zl = land_coords
 
-# FORWARD AND LANDING
-h = drone.get_height()
-print('Before pitch')
-print(h)
+    xs, ys, zs = xs*1000,ys*1000,zs*1000
+    xl, yl, zl = xl*1000,yl*1000,zl*1000
 
-throttle_p = -25
-while h > 50:
-    drone.set_pitch(12)
-    drone.move(1)
-
-    h = drone.get_height()
-    if h <= 50:
-        break
-
-    # drone.set_pitch(0)
-    # drone.move(1)
-    #
-    # h = drone.get_height()
-    # if h <= 50:
-    #     break
-
-    #print(drone.get_height())
-    throttle_p = throttle_p + 3
-    if throttle_p > -15:
-        drone.land()
-        break
-    else:
-        drone.set_throttle(throttle_p + 3)
-        drone.move(1)
-    #print(drone.get_height())
-
-    h = drone.get_height()
-    print('After throttle')
+    h = drone.get_height()  # relative to the ground
+    print('Before pitch')
     print(h)
 
+    pitch_p = zs/(20*3)  # 20 is the scale factor for pitch, assume while loop repeat 3 times
+    # for now assume that camera is on the ground, drone is starting out higher than camera
+    throttle_p = -50 #-ys/9  # 7 is the scale factor for throttle
+    while h > height_threshold:
+        drone.set_pitch(pitch_p)
+        drone.move(1)
+
+        h = drone.get_height()
+        if h <= height_threshold:
+            break
+
+        throttle_p = throttle_p + 3
+        if throttle_p > -15:
+            drone.land()
+            break
+        else:
+            drone.set_throttle(throttle_p)
+            drone.move(1)
+        # print(drone.get_height())
+
+        h = drone.get_height()
+        print('After throttle')
+        print(h)
+
+    drone.land()
+    print("landing")
 
 
-# SQUARE
-# drone.go(Direction.FORWARD, 1, 60)    # moves the drone forward for 2 seconds at 30% power
-# drone.go(Direction.LEFT, 1, 60)            # moves the drone left for 2 seconds at 30 power
-# drone.go(Direction.BACKWARD, 1, 60)    # moves the drone backward for 2 seconds a 30% power
-# drone.go(Direction.RIGHT, 1, 60)            # moves the drone right for 2 seconds at 30% power
+height = drone.get_height()
+print('before takeoff', height)
+drone.takeoff()
+print("taking off")
 
-# TESTING PITCH, ROLL, YAW
-# height = drone.get_height()
-# print(height)
-# angles = drone.get_gyro_angles()
-# print(angles.ROLL, angles.PITCH, angles.YAW)
-# drone.hover(2)
-# height = drone.get_height()
-# print(height)
-# # drone.go(Direction.DOWN, 2, 50)
-# drone.set_roll(-30)
-# angles = drone.get_gyro_angles()
-# print(angles.ROLL, angles.PITCH, angles.YAW)
-# drone.move(1)
-# angles = drone.get_gyro_angles()
-# print(angles.ROLL, angles.PITCH, angles.YAW)
-#
-# #drone.set_roll(0)
-# angles = drone.get_gyro_angles()
-# print(angles.ROLL, angles.PITCH, angles.YAW)
-# #drone.move(1)
-# angles = drone.get_gyro_angles()
-# print(angles.ROLL, angles.PITCH, angles.YAW)
-#
-# drone.set_roll(30)
-# angles = drone.get_gyro_angles()
-# print(angles.ROLL, angles.PITCH, angles.YAW)
-# drone.move(1)
-# angles = drone.get_gyro_angles()
-# print(angles.ROLL, angles.PITCH, angles.YAW)
-# height = drone.get_height()
-# print(height)
-# # drone.hover(2)
-# height = drone.get_height()
-# print(height)
+height = drone.get_height()
+print('after takeoff, before land', height)
 
-# TEST HEIGHT MOVEMENT
-# for i in range(3):
-#     height = drone.get_height()
-#     print(height)
-#     drone.go(Direction.UP, 2, 20)
-#     height = drone.get_height()
-#     if height > 100:
-#         drone.go(Direction.DOWN, 0, 50)
-#     if height < 50:
-#         drone.go(Direction.UP, 2, 50)
-#     # sleep(0.1)
-drone.land()
-# print("Hovering")
-# # drone.set_pitch(45)
-# angles = drone.get_gyro_angles()
-# print(angles.ROLL, angles.PITCH, angles.YAW)
-# drone.hover(10)
-# angles = drone.get_gyro_angles()
-# print(angles.ROLL, angles.PITCH, angles.YAW)# Set positive pitch to 30% power
-# # drone.move(3)            # forward for 2 seconds
-# drone.land()
-print("landing")
+height = drone.get_height()  # in millimeter
+xs,ys,zs = 0,height/1000,1  # in meter
+start_coords = [xs,ys,zs]
+
+straight_land(start_coords)
+height = drone.get_height()
+print('after land', height)
+
 drone.close()
-
